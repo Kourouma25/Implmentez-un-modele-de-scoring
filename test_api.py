904cac7717942790da1c0835c0a5afa10f0eef66
kourouma <kourouma92sekouba@fgmail.com>
2024-12-09 10:55:08 +0100

    mise a jour

--- test_api.py
+++ test_api.py
@@ -5,7 +5,7 @@ import joblib
 
 # Charger les données de test
 data = pd.read_csv('donnee_test/fichier_testeAPI.csv')
-donne_predire = data.iloc[0, :].to_frame()
+donne_predire = data.iloc[0: 1,:]
 # Charger le modèle
 model_enregistre = joblib.load('model_entrainer/lgbm_model.pkl')
 
@@ -18,6 +18,8 @@ model_enregistre = joblib.load('model_entrainer/lgbm_model.pkl')
 def test_predire():
 
     score = model_enregistre.predict_proba(donne_predire)[0]
+    score = score[0]
+
     assert score >= 0 
     assert score <= 1
     """Test pour vérifier la route de prédiction"""
